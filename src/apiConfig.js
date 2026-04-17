17c2d6c5e581ab393c03d0d2ae1bbdb98dd14460
AdamAK-0 <adamabdelkarim123@gmail.com>
2026-04-17 15:42:46 +0300

    _

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,3 +1,3 @@
 // apiConfig.js
-export const API_BASE_URL = "https://ai-music-therapy-backend.onrender.com";
+export const API_BASE_URL = "https://ai-music-therapy-backend.onrender.com/api";
 export const API_PYTHON_URL = "https://ai-music-therapy-server.onrender.com";
