c9df567100150b53a95c6de74ccf98bdf58c941b
AdamAK-0 <adamabdelkarim123@gmail.com>
2026-04-17 19:07:50 +0300

    _

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,4 +2,4 @@
 export const API_BASE_URL = "https://ai-music-therapy-backend.onrender.com/api";
 export const API_PYTHON_URL = "https://ai-music-therapy-server.onrender.com";
 export const API_EMOTION_URL =
-  import.meta.env.VITE_EMOTION_URL || "http://localhost:5004";
+  import.meta.env.VITE_EMOTION_URL || "https://ai-music-therapy-emotiondetection.onrender.com";
